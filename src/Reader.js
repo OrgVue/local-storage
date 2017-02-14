be95f14ace3f40e5bc1422dff7618be0ba7816b2
Rodin van der Hart <rodin.hart@concentra.co.uk>
2017-02-14 14:22:44 +0000

    Catching DB error as well

--- src/Reader.js
+++ src/Reader.js
@@ -2,6 +2,7 @@
 
 // data Reader e a = Reader (e -> a)
 const Reader = run => ({
+  map: f => Reader(e => f(run(e))),
   run: run
 })
 Reader.get = Reader(e => e)
--- src/store.js
+++ src/store.js
@@ -23,44 +23,51 @@ const DBError = (message, key) => ({
 })
 
 // del :: String -> Reader DB (task DBError ())
-const del = key => {
-  return Reader(env => {
-    return Task((rej, res) => {
-      if (!env._db) env._db = levelup(env.name, { db: leveljs })
-
-      env._db.del(key, err => {
-        if (err) rej(DBError(err, key)); else res()
-      })
+const del = key => ensureDB(Reader(env =>
+  Task((rej, res) => {
+    env._db.del(key, err => {
+      if (err) rej(DBError(err, key)); else res()
     })
   })
-}
+))
 
 // ensureDB :: Reader DB (Task DBError a) -> Reader DB (Task DBError a)
-const ensureDB = x => x
+const ensureDB = x => Reader.get.map(env =>
+  Task((rej, res) => {
+    if (!env._db) {
+      levelup(env.name, { db: leveljs }, (err, db) => {
+        if (err) {
+          rej(DBError(err))
+          return          
+        }
+
+        env._db = db
+        x.run(env).fork(rej, res)
+      })
+    } else {
+      x.run(env).fork(rej, res)
+    }
+  })
+)
+
 
 // get :: String -> Reader DB (Task DBError a)
-const get = (key, value) => {
-  return ensureDB(Reader(env => {
-    return Task((rej, res) => {
-      if (!env._db) env._db = levelup(env.name, { db: leveljs })
-      env._db.get(key, (err, value) => {
-        if (err) rej(DBError(err, key)); else res(value)
-      })
-    })
-  }))
-}
-
-// put :: String -> a -> Reader DB (Task DBError ())
-const put = (key, value) => {
-  return Reader(env => {
-    return Task((rej, res) => {
-      if (!env._db) env._db = levelup(env.name, { db: leveljs })
-      env._db.put(key, value, err => {
-        if (err) rej(DBError(err, key)); else res()
-      })
+const get = (key, value) => ensureDB(Reader(env =>
+  Task((rej, res) => {
+    env._db.get(key, (err, value) => {
+      if (err) rej(DBError(err, key)); else res(value)
     })
   })
-}
+))
+
+// put :: String -> a -> Reader DB (Task DBError ())
+const put = (key, value) => ensureDB(Reader(env =>
+  Task((rej, res) => {
+    env._db.put(key, value, err => {
+      if (err) rej(DBError(err, key)); else res()
+    })
+  })
+))
 
 // Exports.
 module.exports = {
