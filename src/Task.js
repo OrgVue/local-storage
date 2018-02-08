b2da0a0a80c0fda5cb179876db5433d2da7265e3
Rodin van der Hart <rodinhart@gmail.com>
2018-02-08 09:46:00 +0000

    Update Task.js

--- src/Task.js
+++ src/Task.js
@@ -7,7 +7,16 @@ const Task = fork => ({
   // ```
   // fork :: (e -> ()) -> (a -> ()) -> ()
   // ```
-  fork: fork
+  fork: fork,
+  
+  // ```
+  // bind :: (a -> Task e b) -> Task e a -> Task e b
+  // ```
+  bind: f => {
+    const m = this
+    
+    return Task((rej, res) => m.fork(rej, x => f(x).fork(rej, res)))
+  }
 })
 
 module.exports = Task
